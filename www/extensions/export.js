227f5adfb8d21bc740d8a3f078a2bd582f19b29e
soitchu <soitchu@proton.me>
2023-05-23 03:30:10 -0700

    Fonts: local

--- www/extensions/export.js
+++
@@ -1,8 +0,0 @@
-// @ts-ignore
-const extensionList = [wco, animixplay, fmovies, zoro, twitch, nineAnime, fmoviesto, gogo, mangaDex, mangaFire];
-// @ts-ignore
-const extensionNames = ["WCOforever", "Animixplay", "FlixHQ", "Zoro", "Twitch", "9anime", "Fmovies.to", "Gogo", "MangaDex", "MangaFire"];
-// @ts-ignore
-const extensionDisabled = [false, true, false, false, false, false, false, false, false, false];
-// @ts-ignore
-const extensionTypes = ["", "", "", "", "", "", "", "", "manga", "manga"];
