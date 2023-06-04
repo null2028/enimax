7f7244ab1ef348950d0409d79073abe425d4cb1f
soitchu <soitchu@proton.me>
2023-06-03 18:01:44 -0700

    Actions: releases when pushed to main

--- helper/uploader.js
+++ helper/uploader.js
@@ -1,6 +1,7 @@
 const dotenv = require("dotenv");
 const { Client, GatewayIntentBits } = require("discord.js");
 const path = require("path");
+const fs = require("fs");
 
 dotenv.config();
 
@@ -15,12 +16,22 @@ async function ini(token, channelID, branch) {
     try {
         await discordClient.login(token);
         const channel = await discordClient.channels.fetch(channelID);
-        await channel.send({
-            content: `${branch} was updated: ${process.env.MESSAGE}`,
-            files: [
-                path.join(__dirname, "../../app-release.apk"),
-            ]
-        });
+
+        if (process.env.RELEASE === "true") {
+            await channel.send({
+                content: `https://github.com/enimax-anime/enimax/releases/latest \n A new version has been released!  : \n\n ${fs.readFileSync(path.join(__dirname, "./releasenotes.txt"), "utf-8")}`,
+                files: [
+                    path.join(__dirname, "../../app-release.apk"),
+                ]
+            });
+        } else {
+            await channel.send({
+                content: `${branch} was updated: ${process.env.MESSAGE}`,
+                files: [
+                    path.join(__dirname, "../../app-release.apk"),
+                ]
+            });
+        }
 
     } catch (err) {
         console.error("Something went wrong");
