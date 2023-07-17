9a3bafab74e307c8d1ad0f4ca13090e86c299dae
soitchu <soitchu@proton.me>
2023-07-17 06:53:31 -0700

    fix(auto-updater): ignore snoozing when switching to  a new update channel

--- src/assets/js/config.ts
+++ src/assets/js/config.ts
@@ -10,7 +10,7 @@ var config : EnimaxConfig = {
 };
 
 localStorage.setItem("version", "1.3.1");
-localStorage.setItem("updatedTime", "1689599541755");
+localStorage.setItem("updatedTime", "1689601576465");
 
 if (localStorage.getItem("lastUpdate") === null) {
     localStorage.setItem("lastUpdate", "0");
--- www/assets/js/config.js
+++ www/assets/js/config.js
@@ -9,7 +9,7 @@ var config = {
     "sockets": false
 };
 localStorage.setItem("version", "1.3.1");
-localStorage.setItem("updatedTime", "1689599541755");
+localStorage.setItem("updatedTime", "1689601576465");
 if (localStorage.getItem("lastUpdate") === null) {
     localStorage.setItem("lastUpdate", "0");
 }
